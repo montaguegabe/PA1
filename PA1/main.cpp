11027c70b773039b4fa31d7f208bda6d9b47890f
Gabe Montague <gmontague@college.harvard.edu>
2017-02-19 19:29:27 -0500

    Initial Commit

---
+++ PA1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PA1
+//
+//  Created by Gabe Montague on 2/19/17.
+//  Copyright © 2017 Gabe Montague. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
